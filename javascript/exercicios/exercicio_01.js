e60c7e3de6ad319cb1232320ca93cee290a2fba3
Maria Cláudia <mariaclaudiapio@gmail.com>
2026-09-17 19:15:02 -0300

    Exercício 01: solução do professor

--- javascript/exercicios/exercicio_01.js
+++ javascript/exercicios/exercicio_01.js
@@ -1,8 +1,11 @@
 /* Escreva um algoritmo para ler o salário mensal atual de um funcionário e o 
     percentual de reajuste. Calcule e escreva o valor do novo salário. */
 
-let salario = 10000.00;
-let reajuste = 0.04;
-let salarioReajustado = (salario * reajuste) + salario;
+let salario = 6000;
+let reajuste = 10;
+aumento = salario * (reajuste/100);
+let salarioReajustado = aumento + salario;
 
 console.log("O valor do salário com o reajuste é: R$" + salarioReajustado + ".");
+
+
