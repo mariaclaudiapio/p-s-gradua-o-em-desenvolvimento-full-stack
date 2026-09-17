fc9c0e3515ad9b720dae1447940b6b3ddde63034
Maria Cláudia <mariaclaudiapio@gmail.com>
2026-09-17 19:23:13 -0300

    exercicio 02

---
+++ javascript/exercicios/exercicio_02.js
@@ -0,0 +1,8 @@
+/* Faça um algoritmo que leia um número inteiro e que imprima o seu sucessor e o 
+    seu antecessor */
+let numero = 10;
+let antecessor = numero - 1;
+let sucessor = numero + 1;
+
+console.log("O antecessor do número " + numero + " é " + antecessor + ".");
+console.log("O sucessor do número " + numero + " é " + sucessor + ".");
